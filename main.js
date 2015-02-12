b205763c2b0f055362721bfb1a113f4a207a720f
Colin LeSage <clesage@starbucks.com>
2015-02-12 12:08:18 -0800

    Revert 1dee156..316ac31
    This rolls back to commit 1dee156462b03a55904203385b01a4ecfe8a1a38.

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var light = new mraa.Aio(0);
 var lightValue;
 var lcdMessage=" ";
 var myLCD = new LCD.Jhd1313m1(6, 0x3E, 0x62);
-var B = 3975;
-var myAnalogPin = new mraa.Aio(0);
 
 function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
@@ -24,38 +22,6 @@ var sp = new SerialPort("/dev/ttyMFD1", {
 var exec = require('child_process').exec,
     child;
 
-function startSensorWatch() {
-    'use strict';
-    setInterval(function () {
-        var a = myAnalogPin.read();
-        //console.log("Analog Pin (A0) Output: " + a);
-        //console.log("Checking....");
-        
-        var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
-        //console.log("Resistance: "+resistance);
-        var celsius_temperature = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;//convert to temperature via datasheet ;
-        //console.log("Celsius Temperature "+celsius_temperature); 
-        var fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 32;
-        var tempf = Math.round(fahrenheit_temperature);
-        
-        console.log("Fahrenheit Temperature: " + tempf);
-        
-        var spawn = require('child_process').spawn,
-            temp_obs = spawn('iotkit-admin', ['observation', 'temperature', celsius_temperature]);
-        
-        temp_obs.stdout.on('data', function(data) {
-            console.log('stdout: ' + data);
-        }); 
-        temp_obs.stderr.on('data', function(data) {
-            console.log('stderr: ' + data);
-        }); 
-        temp_obs.on('close', function(data) {
-            console.log('observation exited with code ' + data);
-        }); 
-//        
-        //socket.emit("message", fahrenheit_temperature);
-    }, 15000);
-}
 
 
 sp.on("data", function (data) {
@@ -74,5 +40,3 @@ sp.on("data", function (data) {
             }
         });
 });
-
-startSensorWatch();
